a10aac552062c02b75f8d5d7ed3549928790bcf6
elciCode <elcira.rpi@gmail.com>
2022-11-02 14:10:16 -0300

    environment api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://api.escuelajs.co'
+  API_URL: 'http://wallet-main.eba-ccwdurgr.us-east-1.elasticbeanstalk.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'https://api.escuelajs.co'
+  API_URL: 'http://wallet-main.eba-ccwdurgr.us-east-1.elasticbeanstalk.com'
 };
 /*
  * For easier debugging in development mode, you can import the following file
