b60252c5cd9765df312c7051fb58ec7d885f3d0f
elciCode <elcira.rpi@gmail.com>
2022-11-02 15:43:19 -0300

    Creando pagina Home y Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth-login/login/login.component';
+import { RegistroComponent } from './pages/auth-registro/registro/registro.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found/page-not-found.component';
 
-const routes: Routes = [  
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegistroComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
