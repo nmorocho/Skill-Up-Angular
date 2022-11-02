4ca44013f59b5736714f70461efe9bd2fd906f50
Carlos Nahuel Morocho <nahuelmorocho@gmail.com>
2022-11-02 16:13:17 -0300

    home path fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,31 +2,35 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth-login/login/login.component';
 import { RegistroComponent } from './pages/auth-registro/registro/registro.component';
+import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found/page-not-found.component';
-
 
 const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
   },
   {
     path: 'register',
-    component: RegistroComponent
+    component: RegistroComponent,
   },
   {
-    path: '**',
-    component: PageNotFoundComponent
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'not-found',
+    component: PageNotFoundComponent,
   },
   {
     path: '',
     redirectTo: '/home',
-    pathMatch: 'full'
-  }
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
