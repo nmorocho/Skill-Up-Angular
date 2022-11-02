f830d877c50b73d6ea7775c0cf8ceed91ce7ff18
Carlos Nahuel Morocho <nahuelmorocho@gmail.com>
2022-11-02 16:56:30 -0300

    user-details component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
 import { SharedModule } from './shared/shared.module';
+import { UserDetailsComponent } from './pages/users/user-details/user-details.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, UserDetailsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
