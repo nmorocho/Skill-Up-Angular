3b43bc00e8740dc08368fde0de9a6a3323ad09ec
elciCode <elcira.rpi@gmail.com>
2022-11-03 13:50:36 -0300

    Vista del login y otros cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { AuthRegistroModule } from './pages/auth-registro/auth-registro.module';
     BrowserAnimationsModule,
     SharedModule,
     AuthRegistroModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent],
