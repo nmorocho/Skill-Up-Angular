7807524477ff2d6e0d58afaa9877df1a30c4fa5d
Richard <71940232+Richulink@users.noreply.github.com>
2022-11-04 20:58:37 -0300

    spinner

---
+++ src/app/spinner/spinner.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpinnerService {
+
+  constructor(private spi: NgxSpinnerService) { }
+
+callshow(){
+this.spi.show()
+}
+
+callhide(){
+  this.spi.hide()
+}
+}
