6834c2cf452a114c61ae055b98dd2486df328278
Carlos Nahuel Morocho <nahuelmorocho@gmail.com>
2022-11-04 11:52:27 -0300

    redirect from class removed

--- src/app/pages/auth-login/login/login.component.ts
+++ src/app/pages/auth-login/login/login.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/services/auth.service';
-import { Router } from '@angular/router';
-import { Auth } from 'src/app/core/interfaces/Auth.interface';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +10,7 @@ import { Auth } from 'src/app/core/interfaces/Auth.interface';
 export class LoginComponent implements OnInit {
   public loginForm: FormGroup = new FormGroup({});
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.initializeForm();
@@ -29,13 +27,6 @@ export class LoginComponent implements OnInit {
   }
 
   onLogin() {
-    this.authService.login(this.loginForm.value).subscribe({
-      next: () => {
-        this.router.navigate(['/home']);
-      },
-      error: (error) => console.log,
-    });
+    this.authService.login(this.loginForm.value).subscribe();
   }
-
-
 }
