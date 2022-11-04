e52f56bd63e02ec4ded7d5fa553ad097824e39f4
Carlos Nahuel Morocho <nahuelmorocho@gmail.com>
2022-11-04 11:48:11 -0300

    duplicated log out button deleted

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,23 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor(
-    private router: Router
-  ) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   logout() {
-    this.router.navigate(['login']);
-    return localStorage.clear();
+    this.authService.logout();
   }
-
 }
