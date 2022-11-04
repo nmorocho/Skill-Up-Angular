82e9a499c45e3c3100dca90fd05ba00fafa8e468
Carlos Nahuel Morocho <nahuelmorocho@gmail.com>
2022-11-04 11:44:48 -0300

    log out method added & login method now redirects

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -8,8 +8,8 @@ import {
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { LoginResponse } from '../interfaces/ApiResponse.interface';
 import { environment } from 'src/environments/environment';
-import { access } from 'fs';
 import { TokenService } from './token.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -17,8 +17,9 @@ import { TokenService } from './token.service';
 export class AuthService {
   constructor(
     private http: HttpClient,
-    private tokenService: TokenService
-    ) {}
+    private tokenService: TokenService,
+    private router: Router
+  ) {}
 
   createUser(user: RegisterBodyUser): Observable<any> {
     return this.http.post(`${environment.API_URL}/users`, user);
@@ -29,9 +30,15 @@ export class AuthService {
       .post<LoginResponse>(`${environment.API_URL}/auth/login`, user)
       .pipe(
         map((res) => {
-          this.tokenService.saveToken(res.accessToken)
+          this.tokenService.saveToken(res.accessToken);
+          this.router.navigate(['home']);
         })
       );
+  }
+
+  logout(): void {
+    this.tokenService.removeToken();
+    this.router.navigate(['login']);
   }
 
   createAcount(token: string): Observable<any> {
