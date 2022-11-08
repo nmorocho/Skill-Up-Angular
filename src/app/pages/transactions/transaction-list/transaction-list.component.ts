b2b856340614a985938d5c0e64290443c4f386c6
marco <you@example.com>
2022-11-08 12:58:22 -0500

    trnasaction list added

--- src/app/pages/transactions/transaction-list/transaction-list.component.ts
+++ src/app/pages/transactions/transaction-list/transaction-list.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Transaction, Transactions } from 'src/app/core/interfaces/Transaction.interface';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-transaction-list',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transaction-list.component.scss']
 })
 export class TransactionListComponent implements OnInit {
-
-  constructor() { }
+  data: Transactions
+  constructor(private http: HttpClient, private authService: AuthService) {
+  }
 
   ngOnInit(): void {
+    this.changePage()
+  }
+
+  changePage(page: string = "/transactions/?page=1") {
+    const customHeaders = new HttpHeaders({
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    });
+    this.http.get<Transactions>(
+      `http://wallet-main.eba-ccwdurgr.us-east-1.elasticbeanstalk.com${page}`,
+      { headers: customHeaders }
+    ).subscribe(info => {
+      console.log(info)
+      this.data = info
+    })
   }
 
 }
+
+
