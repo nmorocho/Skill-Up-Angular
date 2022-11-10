e58610b514c3c61650858fb5acf710f5d1887ce1
marco <you@example.com>
2022-11-10 13:25:37 -0500

    remove and edit in operation service

--- src/app/pages/transactions/transaction-list/transaction-list.component.ts
+++ src/app/pages/transactions/transaction-list/transaction-list.component.ts
@@ -1,38 +1,46 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Transaction, Transactions } from 'src/app/core/interfaces/Transaction.interface';
 import { AuthService } from 'src/app/core/services/auth.service';
+import { OperationsService } from 'src/app/core/services/operations.service';
 
 @Component({
   selector: 'app-transaction-list',
   templateUrl: './transaction-list.component.html',
   styleUrls: ['./transaction-list.component.scss']
 })
-export class TransactionListComponent implements OnInit {
-  data: Transactions
-  constructor(private http: HttpClient, private authService: AuthService) {
-  }
+export class TransactionListComponent {
+  constructor(
+    private http: HttpClient,
+    private operationsService: OperationsService
+  ) { }
+  public editItemId: string = ""
+  get infoTransactions(): Transactions { return this.operationsService.infoTransactions }
 
-  ngOnInit(): void {
-    this.changePage()
+  setEditItemId(id: string) {
+    this.editItemId = id
   }
+  @ViewChild('editConcept',{static:false}) editConcept: ElementRef<HTMLInputElement>
 
-  changePage(page: string = "/transactions/?page=1") {
-    const customHeaders = new HttpHeaders({
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    });
-    this.http.get<Transactions>(
-      `http://wallet-main.eba-ccwdurgr.us-east-1.elasticbeanstalk.com${page}`,
-      { headers: customHeaders }
-    ).subscribe(info => {
-      console.log(info)
-      this.data = info
-    })
-  }
-  modifiDate(date:string){
-    return date.substring(0,10)
-  }
 
+  changePage(page: string) {
+    this.operationsService.viewProfileTransaction(page)
+      .subscribe(console.log)
+  }
+  modifyDate(date: string) {
+    return date.substring(0, 10)
+  }
+  deletetransaction(id: string) {
+    this.operationsService.deleteTransaction(id)
+      .subscribe(console.log)
+  }
+  confirmEdit(button:HTMLButtonElement,transaction:Transaction) {
+    const newConcept = button.parentElement.parentElement.querySelector("input").value
+    transaction.concept = newConcept
+    this.operationsService.editTransaction(this.editItemId,transaction)
+      .subscribe(console.log)
+    this.setEditItemId("")
+  }
 }
 
 
