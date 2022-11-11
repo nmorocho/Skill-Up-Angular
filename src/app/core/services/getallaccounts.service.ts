f5a42dbbe63aac7718fd9e3919d0e1db320530ca
marco <you@example.com>
2022-11-11 16:37:38 -0500

    retiro metodo añadido

--- src/app/core/services/getallaccounts.service.ts
+++ src/app/core/services/getallaccounts.service.ts
@@ -1,18 +1,19 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Account } from '../interfaces/Account.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetallaccountsService {
 
-
+  accountList: Account[] = []
   getAccountById(idAccount: undefined) {
-  {
-    return this.http.get(`${environment.API_URL}/users/${idAccount}`);
-  }
+    {
+      return this.http.get(`${environment.API_URL}/users/${idAccount}`);
+    }
 
 
   }
@@ -27,7 +28,7 @@ export class GetallaccountsService {
 
   public idUser: number | undefined;
   accountSelected: any;
-  
+
   customHeaders = new HttpHeaders({
     Authorization: `Bearer ${this.token}`,
   });
@@ -39,10 +40,14 @@ export class GetallaccountsService {
   sendMoney(idCuenta: any): Observable<any> {
     return this.http.post(`${environment.API_URL}/accounts/`, idCuenta)
   }
-    getRelatedAccount(): Observable<any> {
-      return this.http.get(`${environment.API_URL}/accounts/me`, { headers: this.customHeaders })
-    }
-
-    
+  getRelatedAccount(): Observable<Account[]> {
+    return this.http.get<Account[]>(`${environment.API_URL}/accounts/me`, { headers: this.customHeaders })
+      .pipe(tap(data => { this.accountList = data }))
   }
-  
+
+
+  ChangeMoneyByAccountId(id: number, newBalance:string) {
+    this.accountList = this.accountList.map(account => account.id === id ? {...account,money:newBalance} : account)
+  }
+
+}
