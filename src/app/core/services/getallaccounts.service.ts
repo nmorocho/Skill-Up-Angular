1ef6c4b58ab8138f13a98452234a291050a66b9c
Richard <71940232+Richulink@users.noreply.github.com>
2022-11-11 16:35:46 -0300

    remodelado del metodo del para reutilizar el antiguo para otra funcion

--- src/app/core/services/getallaccounts.service.ts
+++ src/app/core/services/getallaccounts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -8,17 +8,41 @@ import { environment } from 'src/environments/environment';
 })
 export class GetallaccountsService {
 
+
+  getAccountById(idAccount: undefined) {
+  {
+    return this.http.get(`${environment.API_URL}/users/${idAccount}`);
+  }
+
+
+  }
+  idAccount: any;
+  setAccount(account: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  token = environment.TOKEN_KEY;
+
   constructor(private http: HttpClient) { }
 
   public idUser: number | undefined;
-
   accountSelected: any;
+  
+  customHeaders = new HttpHeaders({
+    Authorization: `Bearer ${this.token}`,
+  });
+
 
   getTenAccounts(page: number): Observable<any> {
     return this.http.get(`${environment.API_URL}/accounts?page=${page}`)
   }
   sendMoney(idCuenta: any): Observable<any> {
     return this.http.post(`${environment.API_URL}/accounts/`, idCuenta)
-
   }
-}
+    getRelatedAccount(): Observable<any> {
+      return this.http.get(`${environment.API_URL}/accounts/me`, { headers: this.customHeaders })
+    }
+
+    
+  }
+  
