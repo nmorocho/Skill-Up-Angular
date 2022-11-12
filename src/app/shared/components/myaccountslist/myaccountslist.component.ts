21f10e876893df3f7e5d5098283b984a0d9d9ac6
JuanIsa <JuanIsa>
2022-11-12 19:30:39 -0300

    charge of own money

--- src/app/shared/components/myaccountslist/myaccountslist.component.ts
+++ src/app/shared/components/myaccountslist/myaccountslist.component.ts
@@ -8,38 +8,36 @@ import { AuthService } from 'src/app/core/services/auth.service';
   styleUrls: ['./myaccountslist.component.scss']
 })
 export class MyaccountslistComponent implements OnInit {
-  constructor(private userlistservice: GetallaccountsService, private userinfo: AuthService) { }
+  constructor(private getall: GetallaccountsService, private userinfo: AuthService) { }
   accountList: any[] = [];
   contador: number = 1;
   userId: any;
   isLoading: boolean = true;
   accountListLoad: boolean = false;
+
+  
   ngOnInit(): void {
     this.userinfo.viewProfile().subscribe({ next: data => this.userId = data.id })
-    this.getAllAccounts();
-  }
+    this.getAllUsers();
 
-  getAllAccounts() {
-    this.userlistservice.getRelatedAccount().subscribe({ next: data => console.log(data[0].money)})
+
   }
   
-  setAccount(data: any) {
-    if (this.accountList.some(accountList => accountList.isSelected === true)) {
-      this.accountList.forEach((account, index, array) => {
-        if (account.isSelected) {
-          array[index].isSelected = false;
-        }
-        if (account.id === data.id) {
-          array[index].isSelected = true;
-        }
-      });
-    } else {
-      this.accountList.forEach((account, index, array) => {
-        if (account.id === data.id) {
-          array[index].isSelected = true;
-        }
-      });
-    }
-    localStorage.setItem('userAccountList', JSON.stringify(this.accountList));
+  getAllUsers() {
+    
+    this.getall.getRelatedAccount().subscribe({
+ 
+      next: data => {
+        this.accountList = data;
+        this.accountListLoad = true;
+        this.isLoading= false;
+      // console.log('>>>>>>',this.accountList,'<<<<<<<')
+      }
+    })
+  }
+
+  setAccount(idAccount: any) {
+
+    localStorage.setItem('selectedAccount',JSON.stringify(idAccount)); 
   }
 }
